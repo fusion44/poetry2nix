429d2ce2fd6b3d22b768fb47c1b34a3ef9f6aca7
fusion44 <some.fusion@gmail.com>
2025-02-08 09:51:02 +0100

    test

--- overrides/default.nix
+++ overrides/default.nix
@@ -4090,6 +4090,9 @@ lib.composeManyExtensions [
           sha256 = getRepoHash prev.watchfiles.version;
 
           getCargoHash = version: {
+            "1.0.4".outputHashes = {
+              "notify-6.1.1" = "sha256-lT3R5ZQpjx52NVMEKTTQI90EWT16YnbqphqvZmNpw/I=";
+            };
             "0.24.0".outputHashes = {
               "notify-6.1.1" = "sha256-lT3R5ZQpjx52NVMEKTTQI90EWT16YnbqphqvZmNpw/I=";
             };
